ac996e0be763b33c0f98c3742540176d279dff12
ldfha <sowon12020@gmail.com>
2024-04-14 16:24:37 +0900

    feat:dummy 제거

--- src/dummy/posts.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { postId: 1, title: "게시글1", content: "게시글 1의 내용" },
-    { postId: 2, title: "게시글2", content: "게시글 2의 내용" },
-    { postId: 3, title: "게시글3", content: "게시글 3의 내용" },
-    { postId: 4, title: "게시글4", content: "게시글 4의 내용" },
-]
