eda657b76a65b3b22aae7ae038055691c976ac40
변가원 <byeongawon@byeongawon-ui-noteubug.local>
2024-05-06 21:57:18 +0900

    user 기능 추가

---
+++ src/pages/ChangeNickname.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import arrow from "../assets/ArrowLeft.png";
+import Input from "../components/Input";
+
+const GlobalStyle = createGlobalStyle`
+  * { box-sizing: border-box; }
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.color};
+  }
+`;
+const THEMES = {
+  light: { backgroundColor: "#ffffff", color: "#000000" },
+  dark: { backgroundColor: "#03040c", color: "#ffffff" },
+};
+
+const Container = styled.div`
+  width: 400px;
+  margin: auto;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 30px auto 80px;
+  padding: 0 10px;
+  font-size: 22px;
+`;
+const Arrow = styled.img`
+  width: 24px;
+`;
+const Title = styled.div`
+  flex-grow: 1;
+  text-align: center;
+  margin-right: 15px;
+`;
+const Span = styled(Link)``;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: row;
+  text-align: center;
+  justify-content: center;
+`;
+
+const Button = styled(Link)`
+  background-color: #004a9e;
+  border: none;
+  border-radius: ${({ $round }) => ($round ? `9999px` : `8px`)};
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 0 16px;
+  width: 80px;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 20px 0 0;
+  text-decoration: none;
+
+  &:hover,
+  &:active {
+    background-color: #16457a;
+  }
+`;
+
+function ChangeNickname() {
+  const [newNickname, setNewNickname] = useState("");
+  const BASE_URL = "http://13.209.42.36:4000/api/v1/user";
+  const navigate = useNavigate();
+
+  // 닉네임 수정 함수
+  const updateNickname = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      await axios.patch(
+        `${BASE_URL}/nickname`,
+        { nickname: newNickname },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      alert("닉네임이 성공적으로 변경되었습니다.");
+      navigate("/LoginUserInfo");
+    } catch (error) {
+      alert("닉네임 변경에 실패했습니다.");
+    }
+  };
+  return (
+    <ThemeProvider theme={THEMES["light"]}>
+      <Container>
+        <GlobalStyle />
+        <Header>
+          <Span to="/">
+            <Arrow src={arrow} alt="이전" />
+          </Span>
+          <Title>닉네임 변경</Title>
+        </Header>
+        <Form>
+          <Input
+            type="text"
+            placeholder="새 닉네임"
+            value={newNickname}
+            onChange={(e) => setNewNickname(e.target.value)}
+          />
+          <Button onClick={updateNickname}>변경</Button>
+        </Form>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default ChangeNickname;
---
+++ src/pages/UserInfo.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import { Avatar } from "antd";
+import arrow from "../assets/ArrowLeft.png";
+import profileImg from "../assets/profileImg.png";
+
+const GlobalStyle = createGlobalStyle`
+  * { box-sizing: border-box; }
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    font-family: 'Noto Sans KR', sans-serif;
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.color};
+  }
+`;
+const THEMES = {
+  light: { backgroundColor: "#ffffff", color: "#000000" },
+  dark: { backgroundColor: "#03040c", color: "#ffffff" },
+};
+
+const Container = styled.div`
+  width: 400px;
+  margin: auto;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 30px auto 50px;
+  padding: 0 10px;
+  font-size: 22px;
+`;
+const Arrow = styled.img`
+  width: 24px;
+`;
+const Title = styled.div`
+  flex-grow: 1;
+  text-align: center;
+  margin-right: 15px;
+`;
+const Span = styled(Link)``;
+
+const ProfileImgContainer = styled.div`
+  position: relative;
+  display: inline-block;
+  margin: 0 20px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const UserContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  border-radius: 8px;
+  border: 1px solid #eeeeee;
+  background-color: #f6f6f6;
+  padding: 30px 0;
+  align-items: center;
+  margin: 20px;
+  width: 360px;
+`;
+
+function UserInfo() {
+  const email = "bkw0820@inu.ac.kr";
+  const BASE_URL = `http://13.209.42.36:4000/api/v1/user/${email}`;
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    nickname: "",
+    profileImage: profileImg,
+  });
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(BASE_URL, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        const fetchedProfileImage = response.data.profileImage || profileImg;
+        setUserInfo({
+          email: response.data.email,
+          nickname: response.data.nickname,
+          profileImage: fetchedProfileImage,
+        });
+      } catch (err) {
+        const message =
+          err.response?.data?.message || "Failed to fetch user information.";
+        setError(message);
+      }
+    };
+    fetchUserInfo();
+  }, []);
+
+  return (
+    <ThemeProvider theme={THEMES["light"]}>
+      <Container>
+        <GlobalStyle />
+        <Header>
+          <Span to="/">
+            <Arrow src={arrow} alt="이전" />
+          </Span>
+          <Title>유저 정보</Title>
+        </Header>
+        <Form>
+          <UserContainer>
+            <ProfileImgContainer>
+              <Avatar src={userInfo.profileImage} size={100} />
+            </ProfileImgContainer>
+            <div>
+              <p>닉네임: {userInfo.nickname || "Not provided"}</p>
+              <p>이메일: {userInfo.email || "Not provided"}</p>
+            </div>
+          </UserContainer>
+        </Form>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default UserInfo;
