52a32972740c73bc2f3b2add7ad49324ab3e0b7d
HKYoung-private <gkrbdud1388@gmail.com>
2024-05-08 23:53:14 +0900

    feat: 게시물 마무리 작업

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 import { BottomNavigation } from '../components/BottomNavigation'
 import client from '../lib/client'
 import { useEffect, useState } from 'react'
-import { format } from 'date-fns/format'
 import { ReactComponent as RightArrow } from '../assets/icons/arrow_right.svg'
 import { useNavigate } from 'react-router-dom'
+
+import { PostListItem } from '../components/PostListItem'
 
 export default function Posts() {
   const navigate = useNavigate()
@@ -36,19 +37,7 @@ export default function Posts() {
       </TitleContainer>
 
       <PostList>
-        {posts &&
-          posts.latestList.map(({ postId, title, content, writeDatetime }) => (
-            <li
-              key={postId}
-              onClick={() => {
-                navigate(`/post/${postId}`)
-              }}
-            >
-              <Title>{title}</Title>
-              <Content>{content}</Content>
-              <DateText>{format(new Date(writeDatetime), 'MM.dd')}</DateText>
-            </li>
-          ))}
+        {posts && posts.latestList.map(post => <PostListItem {...post} />)}
       </PostList>
 
       <BottomNavigation />
@@ -73,14 +62,14 @@ const TopBox = styled.div`
   background-color: #f7f7fb;
   border-radius: 16px;
   width: 100%;
-  padding: 24px 16px;
+  padding: 20px 16px;
   display: flex;
   align-items: center;
   justify-content: space-between;
 `
 
 const TopBoxTitle = styled.span`
-  font-size: 20px;
+  font-size: 18px;
   line-height: 24px;
   color: #505050;
   font-weight: 600;
@@ -113,41 +102,4 @@ const PostList = styled.ul`
     padding: 16px;
     border-top: 1px solid #f1f1f5;
   }
-`
-
-const Title = styled.span`
-  font-size: 18px;
-  line-height: 24px;
-  letter-spacing: -0.4px;
-  color: #111111;
-  font-weight: 600;
-  display: inline-block;
-  width: 100%;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  word-break: break-all;
-`
-
-const Content = styled.p`
-  margin: 4px 0px;
-  font-size: 16px;
-  line-height: 24px;
-  letter-spacing: -0.4px;
-  color: #505050;
-  display: inline-block;
-  width: 100%;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  word-break: break-word;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-`
-
-const DateText = styled.span`
-  font-size: 13px;
-  line-height: 18px;
-  letter-spacing: -0.4px;
-  color: #999999;
 `
